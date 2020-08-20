1015ff92131634a596d249a9057c7d385e552a36
wooksbaby <wooksbaby@hotmail.com>
2020-08-20 10:41:07 +0900

    4

--- danjijungbo.py
+++ danjijungbo.py
@@ -1,10 +1,14 @@
 import requests
 
-
+headers = {'Content-Type': 'application/json; charset=utf-8'}
 url = 'http://apis.data.go.kr/1611000/AptListService/getLegaldongAptList'
-queryParams = {'serviceKey' : '3YUl0cFJp1ZmDCE5SOkvsskQu4fTCMlpwz4qdmYz4jjtMrrh9038gesJ8PI5NPPoT79pr0juhvpfjEtSnNbjIg%3D%3D', 'bjdCode' : '2638010100', 'pageNo' : '1', 'numOfRows' : '10' }
+key_raw = '3YUl0cFJp1ZmDCE5SOkvsskQu4fTCMlpwz4qdmYz4jjtMrrh9038gesJ8PI5NPPoT79pr0juhvpfjEtSnNbjIg%3D%3D'
+key = requests.utils.unquote(key_raw)
+bjd_dode = '4119010800'
+queryParams = {'ServiceKey': key, 'bjdCode': bjd_dode, 'pageNo': '1', 'numOfRows': '46'}
 
-request = requests.get(url, params=queryParams)
-requests.Response
-
-print(requests.Response)
+response = requests.get(url, headers=headers, params=queryParams)
+test = response.text
+print(response.url)
+print('test:' + test)
+print('완료')
